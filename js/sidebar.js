c928345cd76babbe17f357b8d00b3af3f04bc312
jccarvalhosa <jocarvalhosa@gmail.com>
2015-11-07 19:54:47 -0200

    mudando forma de diminuir e aumentar sidebar

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
+
   var sidebar_container = $("#sidebar-container");
+  var expand_collapse_div = $("#sidebar-expand-collapse");
 
-  $("#sidebar-close-div").click(function(e) {
-    sidebar_container.removeClass("sidebar-container-bigger").addClass("sidebar-container-smaller");
-    e.stopPropagation();
-  });
-
-  sidebar_container.click(function(e) {
-    if($(e.target).closest('a').length
-      || !sidebar_container.hasClass("sidebar-container-smaller"))
-      return;
-
-    sidebar_container.removeClass("sidebar-container-smaller").addClass("sidebar-container-bigger");
+  expand_collapse_div.click(function() {
+    if(expand_collapse_div.hasClass("collapse-display")) {
+      expand_collapse_div.removeClass("collapse-display").addClass("expand-display");
+      sidebar_container.removeClass("sidebar-container-bigger").addClass("sidebar-container-smaller");
+    }
+    else {
+      expand_collapse_div.removeClass("expand-display").addClass("collapse-display");
+      sidebar_container.removeClass("sidebar-container-smaller").addClass("sidebar-container-bigger");
+    }
   });
 
 });
--- js/topmenu.js
+++ js/topmenu.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  $(".top-buttons-area .normal-display").click(function(e) {
+  $(".top-buttons-area .normal-display").click(function() {
     if($(this).hasClass("normal-display"))
       $(this).removeClass("normal-display").addClass("active-display");
     else
