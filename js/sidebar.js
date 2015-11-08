5416e05ed8e8e7a57172f2936dbcfc4d4771712e
giubpl <giulia.bins@gmail.com>
2015-11-07 23:22:32 -0200

    criando area para a pagina principal

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
 
-  var sidebar_container = $("#sidebar-container");
+  var container = $("#page-container");
   var expand_collapse_div = $("#sidebar-expand-collapse");
 
   expand_collapse_div.click(function() {
     if(expand_collapse_div.hasClass("collapse-display")) {
       expand_collapse_div.removeClass("collapse-display").addClass("expand-display");
-      sidebar_container.removeClass("sidebar-container-bigger").addClass("sidebar-container-smaller");
+      container.removeClass("sidebar-container-bigger").addClass("sidebar-container-smaller");
     }
     else {
       expand_collapse_div.removeClass("expand-display").addClass("collapse-display");
-      sidebar_container.removeClass("sidebar-container-smaller").addClass("sidebar-container-bigger");
+      container.removeClass("sidebar-container-smaller").addClass("sidebar-container-bigger");
     }
   });
 
