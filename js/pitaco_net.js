16483480649d6200434f34b862c7404baea5f196
jccarvalhosa <jocarvalhosa@gmail.com>
2015-11-18 01:36:37 -0200

    refactor

--- js/pitaco_net.js
+++ js/pitaco_net.js
@@ -125,19 +125,10 @@ PitacoDrawerHelper.prototype.drawPitacoNet = function() {
 
 PitacoDrawerHelper.prototype.addZoomerBehaviour = function() {
   var svg = d3.select("#pitaco-net-svg");
-  var box = svg.node().getBBox();
-  var xScale = d3.scale.linear().domain([0, box.width]).range([0, box.width]);
-  var yScale = d3.scale.linear().domain([0, box.height]).range([0,box.height]);
-
   var zoom_group = d3.select("#pitaco-net-zoom-group");
-  var zoomer = d3.behavior.zoom()
-                  .scaleExtent([0.1,10])
-                  .x(xScale)
-                  .y(yScale)
-                  .on("zoom", function() {
-                    zoom_group.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")");
-                  });
-
+  var zoomer = d3.behavior.zoom().scaleExtent([0.1,10]).on("zoom", function() {
+    zoom_group.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")");
+  });
   svg.call(zoomer)
       .on("dblclick.zoom", null)
       //FIXME: this is a hack! Remove when development is finished
