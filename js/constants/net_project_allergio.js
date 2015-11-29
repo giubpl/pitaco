52f772a971663108807d09651b20f5833d351b21
giubpl <giulia.bins@gmail.com>
2015-11-28 22:08:12 -0200

    colocando pitacos funcionais nas branches do projeto allergio

--- js/constants/net_project_allergio.js
+++ js/constants/net_project_allergio.js
@@ -45,10 +45,16 @@
           pitacos:
             [
               {
-                imgs: ["img/fulano.jpg"],
+								imgs: ["img/FE16967CFCAF933B.jpg", "img/B943C01085604582.jpg"],
+								tags: ["Resumo", "Texto", "Puc-Rio"],
                 cx: 551,
                 cy: 218,
-                text: ""
+								text: "OBJETIVO: Apresentar e analisar criticamente as diversas concepções que professores da disciplina educação nutricional têm de atividade prática e como entendem a relação teoria-prática. MÉTODOS: O estudo foi desenvolvido nas Instituições de Ensino Superior do Estado de São Paulo, Brasil, que apresentaram reconhecimento pelo Ministério da Educação até o ano de 1999. A partir dessa informação foram realizadas entrevistas com os docentes da disciplina Educação Nutricional de cada uma dessas Instituições, totalizando 11 sujeitos. Os resultados foram analisados qualitativamente, na perspectiva da hermenêutica-dialética, considerada como um caminho do pensamento. RESULTADOS: As docentes apresentaram pensamentos similares em relação à importância da prática, porém concepções variadas de atividade prática em educação nutricional. Observou-se ainda hipervalorização da prática em detrimento da teoria. Esses aspectos contribuem para comprometer a efetivação da relação teoria-prática.CONCLUSÃO: Cabe aos cursos de graduação e aos órgãos fiscalizadores do ensino, a discussão a respeito da formação dos docentes, da concepção de atividade prática e de formas mais eficazes para que se efetive o movimento dialético da prática com a teoria. É necessário estimular, desde o início da graduação, a articulação entre teoria e prática, a fim de possibilitar aos alunos a contextualização na construção de conhecimentos e formação de compromissos sociais e éticos.",
+								author: {
+									name: "Jonathan Carvalhosa",
+									area: "Desenvolvedor",
+									img: "img/fulano.jpg"
+								}
               },
             ]
         },
@@ -61,10 +67,16 @@
           pitacos:
             [
               {
-                imgs: ["img/fulano.jpg"],
+								imgs: ["img/FE16967CFCAF932F.jpg"],
+								tags: ["Alergia", "Leite", "Amendoim"],
                 cx: 551,
                 cy: 618,
-                text: ""
+                text: "Alergia alimentar é uma reação do sistema imunológico que ocorre logo após a ingestão de um determinado alimento. Mesmo uma pequena quantidade do alimento que causa alergia em algumas pessoas pode desencadear sinais e sintomas, que costumam variar de gravidade. Em alguns casos, a alergia alimentar pode causar sintomas graves ou até mesmo uma reação com risco de vida - conhecida como anafilaxia.",
+								author: {
+									name: "Luiz Rates",
+									area: "Publicitário",
+									img: "img/users_pitaqueiros/04.jpg"
+								}
               },
             ]
         }
